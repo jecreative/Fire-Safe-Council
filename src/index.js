300f3dfdcbd38305d5095073357b9aafd9a20d97
Jordan Esguerra <creative@jordanesguerra.com>
2020-10-11 20:33:38 -0700

    Removed react-snap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 
-import { hydrate, render } from 'react-dom'
+// import { hydrate, render } from 'react-dom'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
-const rootElement = document.getElementById('root')
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement)
-} else {
-  render(<App />, rootElement)
-}
+// const rootElement = document.getElementById('root')
+// if (rootElement.hasChildNodes()) {
+//   hydrate(<App />, rootElement)
+// } else {
+//   render(<App />, rootElement)
+// }
