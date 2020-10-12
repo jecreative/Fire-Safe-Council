ec53b76b78c9dc53fe615c6ae4520f7d09b5fb25
Jordan Esguerra <creative@jordanesguerra.com>
2020-10-11 20:22:40 -0700

    Build attempt 3

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import { hydrate, render } from 'react-dom'
 
 // ReactDOM.render(<App />, document.getElementById('root'));
+//
 
 const rootElement = document.getElementById('root')
 if (rootElement.hasChildNodes()) {
